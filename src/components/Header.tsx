03f71a65f9fc00c195dd7b5ea0bc5d4a7b0965ee
Mandy Ibéné <mandy.ibene@gmail.com>
2025-08-18 16:38:26 +0200

    Added a background to the header so it doesn't blend with the rest when scrolling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,9 +7,12 @@ import Navigation from "./Navigation";
 export default function Header() {
 
   return (
-    <header className="fixed top-0 z-10 px-8 w-full h-[140px] flex justify-between">
+    <header className="fixed top-0 z-10 px-8 w-full h-[140px] flex justify-between bg-stone-50 dark:bg-zinc-900 transition-colors duration-200">
       
-      <div className={`text-4xl flex`}><Link href="/" className="self-center">Mandy I.</Link></div>
+      <div className={`text-4xl flex`}>
+        <Link href="/" className="md:hidden self-center">MI.</Link>
+        <Link href="/" className="hidden md:inline self-center">Mandy I.</Link>
+        </div>
       
       <Navigation />
       
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -26,15 +26,15 @@ function Navigation() {
   
   const navClassName = `flex`;
   
-  const liClassName = `opacity-100 rounded-xs ${hoverBgColors[color1]}   dark:hover:bg-stone-50`;
+  const liClassName = `opacity-100 rounded-xs ${hoverBgColors[color1]} dark:hover:bg-stone-50`;
   
   const linkClassName = `group block p-2 uppercase font-semibold text-xl lg:text-lg ${textColors[color3]} dark:text-stone-50 ${hoverTextColors[color2]} dark:hover:text-zinc-900`;
 
   const logoClassName = `fill-zinc-800 group-hover:fill-stone-50 dark:fill-stone-50 dark:group-hover:fill-zinc-800`;
   const logoSize = 20;
   
-  const ulWrapperClassName = `absolute top-0 -right-[100vw] flex flex-col justify-center items-center bg-stone-300/50 backdrop-blur-sm w-screen h-screen lg:size-auto p-4 rounded-xs ${!isMenuClosed && '-translate-x-[100vw]'} lg:top-8 lg:right-1/2 lg:translate-x-1/2 transition-transform duration-500  `
-  const ulClassName = `flex flex-col lg:flex-row justify-center items-center size-[40%] lg:size-auto gap-4 lg:gap-0 bg-stone-50 dark:bg-zinc-900 rounded-xs`;
+  const ulWrapperClassName = `absolute top-0 -right-[100vw] flex flex-col justify-center items-center bg-stone-200/50 w-screen h-screen lg:size-auto p-4 rounded-xs ${!isMenuClosed && '-translate-x-[100vw]'} lg:top-8 lg:right-1/2 lg:translate-x-1/2 transition-transform duration-500`
+  const ulClassName = `flex flex-col lg:flex-row justify-center items-center w-[80%] h-[50%] sm:w-[60%] sm:h-[60%] lg:size-auto p-4 lg:p-0 gap-4 lg:gap-0 bg-stone-50 dark:bg-zinc-900 rounded-xs`;
 
   const burgerPosition = `absolute top-1/2 right-10 transform -translate-y-1/2`;
   const burgerColors = `bg-zinc-800 dark:bg-stone-50`;
@@ -42,6 +42,7 @@ function Navigation() {
   return (
     <nav className={navClassName}>
       
+      {/* links */}
       <div className={ulWrapperClassName}>
         <ul className={ulClassName}>
           <li className={liClassName}>
@@ -59,11 +60,13 @@ function Navigation() {
         </ul>
       </div>
 
+      {/* dark mode toggler */}
       <div className="relative w-16 h-8 mb-1 me-20 lg:me-0 self-center">
         <input defaultChecked={darkMode} id="switch-component" type="checkbox" onChange={toggleDarkMode} className="peer appearance-none w-16 h-8 bg-zinc-900 rounded-full checked:bg-stone-50 cursor-pointer transition-colors duration-300" />
         <label htmlFor="switch-component" className="absolute top-0 left-0 w-8 h-8 bg-stone-50 rounded-full border-2 border-zinc-900 shadow-sm transition-transform duration-300 peer-checked:bg-zinc-900 peer-checked:border-stone-50 peer-checked:translate-x-8 cursor-pointer"></label>
       </div>
 
+      {/* burger button */}
       <BurgerCrossButton responsive={"lg:hidden"} position={burgerPosition} color={burgerColors} isIconBurger={isMenuClosed} clickedOnce={clickedOnce} onClick={toggleMenu} />
 
     </nav>
